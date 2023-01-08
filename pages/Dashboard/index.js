f4547a9dd094267e90e43c284504479b2be315cc
Thomas Truong <thomas.truong01@gmail.com>
2023-01-07 18:34:45 -0800

    Update dashboard

--- pages/Dashboard/index.js
+++ pages/Dashboard/index.js
@@ -15,6 +15,7 @@ import RecentTransactions from '../../components/dashboard/RecentTransactions'
 import DownloadReports from '../../components/dashboard/DownloadReports'
 import Campagin from '../../components/dashboard/Campagin'
 import SalesQuantity from '../../components/dashboard/SalesQuantity'
+import Geography from '../../components/dashboard/Geography'
 
 const Dashboard = () => {
   const theme = useTheme()
@@ -85,21 +86,7 @@ const Dashboard = () => {
         <Campagin />
 
         <SalesQuantity />
-        <Box
-          gridColumn='span 4'
-          gridRow='span 2'
-          backgroundColor={colors.primary[400]}
-          p='30px'
-          borderRadius='1em'
-          sx={{ boxShadow: 3 }}
-        >
-          <Typography variant='h5' fontWeight='600' sx={{ mb: '15px' }}>
-            Geography Based Traffic
-          </Typography>
-          <Box height='200px'>
-            <GeographyChart isDashboard={true} />
-          </Box>
-        </Box>
+        <Geography />
       </Box>
     </Box>
   )
