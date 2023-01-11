dfd8893ca0bff7b487ed55e5be0a4f6b95b706e2
Thomas Truong <thomas.truong01@gmail.com>
2023-01-11 09:02:33 -0800

    Add vitals

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import reportWebVitals from '../lib/reportWebVitals'
+import { sendToVercelAnalytics } from '../lib/vitals'
 
 function MyApp ({ Component, pageProps }) {
   return (
@@ -8,5 +10,5 @@ function MyApp ({ Component, pageProps }) {
     </Layout>
   )
 }
-
+reportWebVitals(sendToVercelAnalytics)
 export default MyApp
